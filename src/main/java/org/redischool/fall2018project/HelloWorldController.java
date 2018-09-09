35d8a29e15931138c28ae31ddc2ea545993c575e
Bradford Hovinen <hovinen@gmail.com>
2018-09-09 20:52:03 +0200

    Organized imports

--- src/main/java/org/redischool/fall2018project/HelloWorldController.java
+++ src/main/java/org/redischool/fall2018project/HelloWorldController.java
@@ -1,12 +1,8 @@
 package org.redischool.fall2018project;
 
-import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.google.common.base.MoreObjects.firstNonNull;
 
 @RestController
 public final class HelloWorldController {
